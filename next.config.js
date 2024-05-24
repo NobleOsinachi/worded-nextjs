25199c1c6c7772cd5bb221d0743ff397a98a43da
Noble Osinachi <nobleosinachi98@gmail.com>
2024-05-24 01:12:28 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -1,33 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
     async headers() {
         return [
             {
-                // Routes this applies to
-                source: "/api/(.*)",
-                // Headers
+                // matching all API routes
+                source: "/api/:path*",
                 headers: [
-                    // Allow for specific domains to have access or * for all
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: "*",
-                        // DOES NOT WORK
-                        // value: process.env.ALLOWED_ORIGIN,
-                    },
-                    // Allows for specific methods accepted
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET, POST, PUT, DELETE, OPTIONS",
-                    },
-                    // Allows for specific headers accepted (These are a few standard ones)
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "Content-Type, Authorization",
-                    },
-                ],
-            },
-        ];
-    },
-};
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig;
